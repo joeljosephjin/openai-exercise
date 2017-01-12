bea66d9e2d10bb1b45fb6c6a28bc4f9dee4bd312
Qiaonan Duan <geonann@gmail.com>
2017-01-11 19:08:27 -0800

    able to run

--- policyGradient.py
+++ policyGradient.py
@@ -1,35 +1,38 @@
 import tensorflow as tf
 
-with name_scope('input'):
+value_lr = 0.01
+target_lr = 0.01
+
+with tf.name_scope('input'):
     observation = tf.placeholder(tf.float32, [None,4], 'observation')
     target = tf.placeholder(tf.float32, [None,1], 'target')
-    with name_scope('action_net'):
+    with tf.name_scope('action_net'):
         action = tf.placeholder(tf.float32, [None,2], 'action')
 
-with name_scope('value_net'):
+with tf.name_scope('value_net'):
     weights_v1 = tf.Variable(tf.truncated_normal([4,10], stddev=0.1),
     name='weights_v1')
-    biases_v1 = tf.Variable(0.1, shape=[10], name='biases_v1')
+    biases_v1 = tf.Variable(tf.constant(0.1, shape=[10]), name='biases_v1')
     preactivation_v1 = tf.matmul(observation, weights_v1) + biases_v1
     activation_v1 = tf.nn.relu(preactivation_v1, name='activation_v1')
 
     weights_v2 = tf.Variable(tf.truncated_normal([10,1], stddev=0.1),
     name='weights_v2')
-    biases_v2 = tf.Variable(0.1, name='biases_v2')
+    biases_v2 = tf.Variable(tf.constant(0.1), name='biases_v2')
     value = tf.matmul(activation_v1, weights_v2) + biases_v2
     mse = tf.contrib.losses.mean_squared_error(value, target)
-    train_value = tf.train.AdamOptimizer(learning_rate=0.001)\
+    train_value = tf.train.AdamOptimizer(learning_rate=value_lr)\
     .minimize(mse)
 
-with name_scope('action_net'):
+with tf.name_scope('action_net'):
     weights_a1 = tf.Variable(tf.truncated_normal([4,1], stddev=0.1),
     name='weights_a1')
     #biases_a1 = tf.Variable(0.1 , name='biases_a1')
     preactivation_a1 = tf.matmul(observation, weights_a1) # + biases_a1
     # probability for action == 0
-    action_prob = tf.sigmoid(preactivation_a1, name='action_prob')
+    action_prob = tf.squeeze(tf.sigmoid(preactivation_a1, name='action_prob'))
 
-    with name_scope('gradient'):
+    with tf.name_scope('gradient'):
         sigmoid_derivative_0 = tf.sigmoid(-preactivation_a1)
         sigmoid_derivative_1 = -tf.sigmoid(preactivation_a1)
         sigmoid_derivative = tf.concat(1,
@@ -37,7 +40,7 @@ with name_scope('action_net'):
         sigmoid_derivative_by_action = tf.multiply(sigmoid_derivative,action)
         sigmoid_derivative_by_action = tf.reduce_sum(
         sigmoid_derivative_by_action,axis=1,keep_dims=True)
-        log_gradient_weights = tf.multiply(sigmoid_derivative_by_action, observation)
+        log_gradient_weights = tf.multiply(sigmoid_derivative_0, observation)
 
         ## could use compute_gradients to calculate gradients automatically
         # logGradient0 = tf.train.Optimizer\
@@ -49,6 +52,11 @@ with name_scope('action_net'):
 
         #log_gradient_biases = sigmoid_derivative_by_action
         advantage = target - value
-        gradient = tf.reduce_mean(tf.multiply(log_gradient_weights,advantage),axis=0)
-        train_action = tf.train.AdamOptimizer(learning_rate=0.001)\
-        .apply_gradients([(gradient,weights_a1)])
+        gradient = tf.reduce_mean(tf.multiply(log_gradient_weights,advantage),
+        axis=0)
+        gradient = tf.expand_dims(gradient, axis=1)
+        #train_action = tf.Print(gradient,[gradient,weights_a1], message='print: ', summarize=5)
+        train_action = weights_a1.assign(weights_a1+target_lr*gradient)
+        #gradient = tf.Print(gradient,[gradient,weights_a1], message='print: ', summarize=5)
+        # train_action = tf.train.AdamOptimizer(learning_rate=target_lr)\
+        # .apply_gradients([(gradient,weights_a1)])
--- run.py
+++ run.py
@@ -4,43 +4,55 @@ import shutil
 import os
 import numpy as np
 import qn
+import tensorflow as tf
 
 
 from policyGradient import observation, target, action, \
-action_prob, train_value, train_action
+action_prob, train_value, train_action #gradient
 
 sess = tf.InteractiveSession()
-sess.run(tf.initialize_all_variables())
+sess.run(tf.global_variables_initializer())
 
 env = gym.make('CartPole-v0')
-logDir = 'tmp/cartpole-experiment-1'
-if os.path.isdir(logDir):
-    shutil.rmtree(logDir)
-env = wrappers.Monitor(env, logDir)
+# logDir = 'tmp/cartpole-experiment-1'
+# if os.path.isdir(logDir):
+#     shutil.rmtree(logDir)
+# env = wrappers.Monitor(env, logDir)
 
 gamma = 0.9
 batch_size = 10
 keep_size = 20
-memory = []
-for i_episode in range(200):
+
+for i_episode in range(500):
     obs = env.reset()
+    memory = []
     for t in range(500):
         #env.render()
         #print(observation)
-        action_p = sess.run(action_prob, feed_dict={observation:obs})
-        action = 0 if action_p > np.random.rand() else 1
-        obs, reward, done, info = env.step(action)
-        memory.append([obs,action,reward])
+        action_p = sess.run(action_prob, feed_dict={observation:[obs]})
+        a = 0 if action_p > np.random.rand() else 1
+        obs, reward, done, info = env.step(a)
+        memory.append([obs,a,reward])
         if done:
             print("{}th episode finished after {} timesteps"\
             .format(i_episode, t+1))
             break
 
-    for i, unit in memory:
-        G = [gamma**j*item[2] for item in memory[i:]]
+    for i, unit in enumerate(memory):
+        G = np.sum([gamma**j*item[2] for j,item in enumerate(memory[i:])])
         unit.append(G)
 
     if len(memory) > keep_size:
         memory = memory[-keep_size:]
 
-    for chunk in
+    for chunk in qn.chunks(memory,batch_size):
+        obsChunk, actionChunk, _, targetChunk = list(zip(*chunk))
+        obsChunk = np.stack(obsChunk)
+        targetChunk = [[x] for x in targetChunk]
+        actionChunk = [[1,0] if x==0 else [0,1] for x in actionChunk]
+        sess.run(train_action,feed_dict={observation:obsChunk,
+        action:actionChunk, target:targetChunk})
+        # sess.run(gradient,feed_dict={observation:obsChunk,
+        # action:actionChunk, target:targetChunk})
+        sess.run(train_value,feed_dict={observation:obsChunk,
+        target:targetChunk})
